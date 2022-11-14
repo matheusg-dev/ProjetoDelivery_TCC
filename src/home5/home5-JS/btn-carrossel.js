5ae61cc3ae24d0c960d9e7a63246cc68ce88048a
Jhon Light <jhony.mluz@gmail.com>
2022-11-13 22:23:43 -0300

    Refatorando carrossel pag 5 html/js

--- src/home5/home5-JS/btn-carrossel.js
+++ src/home5/home5-JS/btn-carrossel.js
@@ -1,8 +1,16 @@
 const btnRecomendado = document.querySelectorAll("[data-btnRecomendado]");
+const btnPopular = document.querySelectorAll("[data-btnPopular]");
+const btnFavorito = document.querySelectorAll("[data-btnFavorito]");
+
 const combinado = document.querySelectorAll(".combinado");
 
 let combinadoCarrossel = combinado[0].children;
+let popularCarrossel = combinado[1].children;
+let favoritoCarrossel = combinado[2].children;
+
 let maxCombinado = combinadoCarrossel.length;
+let maxCombinadoPopular = popularCarrossel.length;
+let maxCombinadoFavorito = favoritoCarrossel.length;
 
 let itemCarrosselAtual = 0;
 
@@ -25,6 +33,58 @@ btnRecomendado.forEach((elementos) => {
     }
 
     combinadoCarrossel[itemCarrosselAtual].scrollIntoView({
+      inline: "center",
+      behavior: "smooth",
+      block: "nearest",
+    });
+  });
+});
+
+btnPopular.forEach((elementos) => {
+  elementos.addEventListener("click", () => {
+    const btnEsquerdo = elementos.classList.contains("btn__esquerda");
+
+    if (btnEsquerdo) {
+      itemCarrosselAtual -= 3;
+    } else {
+      itemCarrosselAtual += 3;
+    }
+
+    if (itemCarrosselAtual >= maxCombinadoPopular) {
+      itemCarrosselAtual = 0;
+    }
+
+    if (itemCarrosselAtual < 0) {
+      itemCarrosselAtual = maxCombinadoPopular - 1;
+    }
+
+    popularCarrossel[itemCarrosselAtual].scrollIntoView({
+      inline: "center",
+      behavior: "smooth",
+      block: "nearest",
+    });
+  });
+});
+
+btnFavorito.forEach((elementos) => {
+  elementos.addEventListener("click", () => {
+    const btnEsquerdo = elementos.classList.contains("btn__esquerda");
+
+    if (btnEsquerdo) {
+      itemCarrosselAtual -= 3;
+    } else {
+      itemCarrosselAtual += 3;
+    }
+
+    if (itemCarrosselAtual >= maxCombinadoFavorito) {
+      itemCarrosselAtual = 0;
+    }
+
+    if (itemCarrosselAtual < 0) {
+      itemCarrosselAtual = maxCombinadoFavorito - 1;
+    }
+
+    favoritoCarrossel[itemCarrosselAtual].scrollIntoView({
       inline: "center",
       behavior: "smooth",
       block: "nearest",
