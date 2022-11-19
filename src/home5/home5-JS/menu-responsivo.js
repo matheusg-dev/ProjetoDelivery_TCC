8002ab29e8a769e194c20037fac228e4941380e8
Jhon Light <jhony.mluz@gmail.com>
2022-11-19 01:29:37 -0300

    Menu responsivo completo pag 5

--- src/home5/home5-JS/menu-responsivo.js
+++ src/home5/home5-JS/menu-responsivo.js
@@ -1,21 +1,24 @@
 const btnMenu = document.querySelector(".btn__menu");
 const dataMenu = document.querySelector("[data-menu]");
+const filtroApi = document.querySelector("[data-filtro]");
 
-/* function mudouTamanho() {
-  if (window.innerWidth >= 540) {
-    dataMenu.classList.add("block");
+function mudouTamanho() {
+  if (window.innerWidth >= 553) {
+    dataMenu.classList.add("textos__menu");
   }
 
-  if (window.innerWidth < 540) {
-    dataMenu.classList.replace("block", "none__btn-menu");
+  if (window.innerWidth < 553) {
+    dataMenu.classList.replace("absolut__menu", "textos__menu");
+    filtroApi.classList.remove("z-index");
   }
-} */
+}
 
 btnMenu.addEventListener("click", () => {
-  if (dataMenu.classList.contains("menu")) {
-    dataMenu.classList.replace("menu", "absolut__menu");
+  if (dataMenu.classList.contains("textos__menu")) {
+    dataMenu.classList.replace("textos__menu", "absolut__menu");
+    filtroApi.classList.add("z-index");
   } else {
-    dataMenu.classList.add("none");
-    dataMenu.classList.replace("none", "menu");
+    dataMenu.classList.replace("absolut__menu", "textos__menu");
+    filtroApi.classList.remove("z-index");
   }
 });
