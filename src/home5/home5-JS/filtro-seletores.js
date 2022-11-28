1f112b66724727c3b75bcaf425e9d7b092f52cf4
jhony.mluz@gmail.com <jhony.mluz@gmail.com>
2022-11-27 21:03:33 -0300

    Add função do botão TUDO pag 5

--- src/home5/home5-JS/filtro-seletores.js
+++ src/home5/home5-JS/filtro-seletores.js
@@ -4,25 +4,35 @@ const displayTudo = document.querySelector("[data-filtrotudo]");
 const prato = document.querySelectorAll("[data-prato]")
 
 btnSelecao[0].onclick = function () {
-  deletaPrincipal.classList.add("none");
+  
+  
+  if (deletaPrincipal.classList.contains("verdadeiro")) {
+    deletaPrincipal.classList.replace("verdadeiro", "none");
 
-  if (deletaPrincipal.classList.contains("none")) {
-    displayTudo.classList.remove("none");
-
-    let tituleH2 = document.createElement("h2");
-    tituleH2.classList.add("recomendado", "popular");
-    tituleH2.innerHTML = "Todos os pratos";
-    displayTudo.appendChild(tituleH2);
+    displayTudo.classList.replace("none", "grid");
+    displayTudo.classList.add("selecao__filtros")
 
     let containerTudo = document.createElement("section")
-    containerTudo.classList.add("flex", "combinado")
+    containerTudo.classList.add("grid", "selecao__filtros")
     deletaPrincipal.appendChild(containerTudo)
 
- /*    console.log(containerTudo)
-    containerTudo.array.forEach(element => {
-
-    });
- */
+    prato.forEach((pratos) => {
+       const clone = pratos.cloneNode(true)
+       clone.classList.add("remove")
+       displayTudo.appendChild(clone)
+    })
   } else {
+    displayTudo.classList.replace("grid", "none")
+    displayTudo.classList.remove("selecao__filtros")
+    deletaPrincipal.classList.remove("none")
+
+    const remove = document.querySelectorAll(".remove")
+
+    remove.forEach((elemento) => {
+      elemento.remove()
+      console.log(elemento)
+    })
+
+    deletaPrincipal.classList.add("verdadeiro")
   }
 };
