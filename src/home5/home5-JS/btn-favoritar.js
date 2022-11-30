3bd1af82880838eb431d4ecb41074c200458618f
Matheus Gomes <matheuzgomes82@gmail.com>
2022-11-30 16:44:00 -0300

    Função de favoritar

--- src/home5/home5-JS/btn-favoritar.js
+++ src/home5/home5-JS/btn-favoritar.js
@@ -1,3 +1,17 @@
 const btnCoracao = document.querySelectorAll(".prato__coracao")
+const  
 
-console.log(btnCoracao)
+btnCoracao.forEach((elemento) => {
+    elemento.onclick = function (event) {
+        if (this.classList.contains("vazio")) {
+            let verifica = event.target.src = "../../assets/img/coracao-red.svg";
+            this.classList.replace("vazio", "favorito")
+
+                        
+        } else {
+            this.classList.replace("favorito", "vazio")
+            let verifica = event.target.src = "../../assets/img/coracao.svg";
+        }
+    }
+})
+
