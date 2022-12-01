98ce1faeb71b9d2c2ec36983a26aa320c243758d
JhonLight <jhony.mluz@gmail.com>
2022-12-01 17:12:13 -0300

    Função de favoritar em andamento

--- src/home5/home5-JS/btn-favoritar.js
+++ src/home5/home5-JS/btn-favoritar.js
@@ -1,5 +1,6 @@
 const btnCoracao = document.querySelectorAll(".prato__coracao")
-const  
+const containerFavoritos = document.querySelector("[data-favorito]")
+
 
 btnCoracao.forEach((elemento) => {
     elemento.onclick = function (event) {
@@ -7,6 +8,28 @@ btnCoracao.forEach((elemento) => {
             let verifica = event.target.src = "../../assets/img/coracao-red.svg";
             this.classList.replace("vazio", "favorito")
 
+
+           prato.forEach((addClass) => {
+            
+            addClass.onclick = function ()
+            addClass.classList.add("CoracaoCheio")
+
+         
+            }) 
+
+ 
+
+              
+           
+           
+            /* prato.forEach((pratoFavorito) => {
+                const clone = pratoFavorito.cloneNode(true)
+                containerFavoritos.appendChild(clone)
+
+                console.log(pratoFavorito)
+            }) */ 
+
+            
                         
         } else {
             this.classList.replace("favorito", "vazio")
