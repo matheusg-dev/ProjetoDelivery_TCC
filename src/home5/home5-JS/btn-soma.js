f04057092b08bf08d9dfc894547cfe32e715fbc8
jhony.mluz@gmail.com <jhony.mluz@gmail.com>
2022-12-05 05:24:54 -0300

    Rota do prato e botão de soma concluido

---
+++ src/home5/home5-JS/btn-soma.js
@@ -0,0 +1,27 @@
+const valorTodos = document.querySelectorAll("[data-contador]")
+const valorPrato = document.querySelectorAll(".between")
+const btnImg = document.querySelectorAll(".prato__img")
+
+valorTodos.forEach((valorInicio) =>{
+    valorInicio.innerHTML = "10,00"
+}) 
+
+valorPrato.forEach((alteraValor) => {
+    alteraValor.onclick = function (elmentoValor) {
+
+        let valor = parseInt(alteraValor.children[0].textContent)
+        alterado = valor + 10 + ",00"
+        alteraValor.children[0].textContent = alterado
+            
+        btnImg.forEach((rotaAtiva) => {
+            rotaAtiva.onclick = function () {
+                let naoAlterado = rotaAtiva.parentElement.childNodes[7].innerText
+                    
+                if (naoAlterado === alterado) {
+                    
+                    window.location.assign("../orderListA6/orderListA6.html")
+                }
+            }
+        })
+    }
+})
