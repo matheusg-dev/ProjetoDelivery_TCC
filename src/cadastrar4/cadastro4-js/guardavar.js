a86c376ff03a5732b04ef86c58a881607f3bdcb9
jhony.mluz@gmail.com <jhony.mluz@gmail.com>
2022-12-07 13:51:45 -0300

    Refatorando guad.js

---
+++ src/cadastrar4/cadastro4-js/guardavar.js
@@ -0,0 +1,86 @@
+/*
+let btn = document.querySelector("#send");
+let formulario = new Map();
+
+btn.addEventListener("click", function(e){
+
+    e.preventDefault();
+
+    const nome = document.querySelector("#PrimeiroNome");
+    formulario.set("Nome", nome.value);
+
+    const email = document.querySelector("#email");
+    formulario.set("Email",  email.value);
+
+
+    const senha = document.querySelector("#senha");
+    formulario.set("Senha", senha.value);
+
+
+    const repetirSenha = document.querySelector("#repetirSenha");
+    formulario.set("RepetirSenha", repetirSenha.value);
+    
+
+    const cpf = document.querySelector("#cpf");
+    formulario.set("Cpf", cpf.value);
+
+
+    const telefone = document.querySelector("#telefone");
+    formulario.set("Telefone", telefone.value);
+  
+
+    const cep = document.querySelector("#cep");
+    formulario.set("Cep", cep.value);
+  
+
+    const endereco = document.querySelector("#endereco");
+    formulario.set("Endereco", endereco.value);
+
+
+    const numero = document.querySelector("#número");
+    formulario.set("Numero", numero.value);
+
+
+    const bairro = document.querySelector("#bairro");
+    formulario.set("Bairro", bairro.value);
+
+    var user = {
+        UsuarioId: 0,
+        UsuarioPrimeiroSome: nome.value,
+        UsuarioEmail: email.value,
+        UsuarioSenha: senha.value,
+        UsuarioRepetirSenha: repetirSenha.value,
+        UsuarioCpf: cpf.value,
+        UsuarioTelefoneWhatsapp: telefone.value,
+        UsuarioCep:cep.value,
+        UsuarioEndereco: endereco.value,
+        UsuarioNumeroCasa: numero.value,
+        UsuarioBairro: bairro.value
+    }
+    
+    post("Usuario/cadastrar4", user)
+        .then(response => console.log(response))
+
+});
+
+function post(endpoint, body)
+{
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    return fetch("https://localhost:7180/" + endpoint, { 
+        method: 'POST',
+        headers: myHeaders,
+        mode: 'cors',
+        cache: 'default',
+        body: JSON.stringify(body)
+    })
+    .then(response => response.json())
+}
+
+function imprimeDados() {
+for (var [key, value] of formulario) {
+
+    console.log(key + " = " + value);
+}}
+*/
