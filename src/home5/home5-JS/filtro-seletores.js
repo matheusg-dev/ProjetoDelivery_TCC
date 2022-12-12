61b9d9312951c4ec59606ebe0ca181c96944c19d
jhony.mluz@gmail.com <jhony.mluz@gmail.com>
2022-12-12 05:54:32 -0300

    Add filtro combo de salada pag 5

--- src/home5/home5-JS/filtro-seletores.js
+++ src/home5/home5-JS/filtro-seletores.js
@@ -3,6 +3,39 @@ const deletaPrincipal = document.querySelector("[data-deleta]");
 const displayTudo = document.querySelector("[data-filtrotudo]");
 const prato = document.querySelectorAll("[data-prato]")
 
+btnSelecao[1].onclick = function () {
+  
+  
+  if (deletaPrincipal.classList.contains("verdadeiro")) {
+    deletaPrincipal.classList.replace("verdadeiro", "none");
+
+    displayTudo.classList.replace("none", "grid");
+    displayTudo.classList.add("selecao__filtros")
+
+    let containerTudo = document.createElement("section")
+    containerTudo.classList.add("grid", "selecao__filtros")
+    deletaPrincipal.appendChild(containerTudo)
+
+    prato.forEach((pratos) => {
+       const clone = pratos.cloneNode(true)
+       clone.classList.add("remove")
+       displayTudo.appendChild(clone)
+    })
+  } else {
+    displayTudo.classList.replace("grid", "none")
+    displayTudo.classList.remove("selecao__filtros")
+    deletaPrincipal.classList.remove("none")
+
+    const remove = document.querySelectorAll(".remove")
+
+    remove.forEach((elemento) => {
+      elemento.remove()
+    })
+
+    deletaPrincipal.classList.add("verdadeiro")
+  }
+};
+
 btnSelecao[0].onclick = function () {
   
   
