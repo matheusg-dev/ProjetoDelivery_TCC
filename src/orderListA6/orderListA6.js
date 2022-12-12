dafdf19b543ba7aa9950fd7ed6bac539bceabe2c
jhony.mluz@gmail.com <jhony.mluz@gmail.com>
2022-12-12 02:51:09 -0300

    Pag perfil finalizada js

--- src/orderListA6/orderListA6.js
+++ src/orderListA6/orderListA6.js
@@ -1,2 +1,13 @@
+window.onload = function() {
+    const containerCarrinho = document.querySelector("[data-carrinho]")
+    const nomePrato = document.querySelector(".salada")
+    
+    var usuarioAtivo = window.localStorage.getItem("usuarioAtivo")
+    let arrStorageObjeto = JSON.parse(usuarioAtivo)
 
+    let nomeImgLogin = arrStorageObjeto[1].nomeImgLogin
+
+    nomePrato.innerHTML = nomeImgLogin
+
+}
 
--- src/perfil11/perfil.js
+++ src/perfil11/perfil.js
@@ -13,5 +13,52 @@ window.onload = function(){
     const bairro = document.querySelector(".label__bairro");
 
     
+    var dadosObjetoUsuario = window.localStorage.getItem("dadosObjetoUsuario")
+    let arrStorageObjeto = JSON.parse(dadosObjetoUsuario)
+
+    let primeiroNome = arrStorageObjeto[0].primeiroNome
+    let pemail = arrStorageObjeto[0].email
+    let pcpf = arrStorageObjeto[0].cpf
+    let ptelefoneWhats = arrStorageObjeto[0].telefoneWhats
+    let pcep = arrStorageObjeto[0].cep
+    let pendereco = arrStorageObjeto[0].endereco
+    let pnumero = arrStorageObjeto[0].numero
+    let pbairro = arrStorageObjeto[0].bairro
+
+    let pNome = document.createElement("p")
+    pNome.innerHTML = primeiroNome
+    nome.appendChild(pNome)
+
+    let cemail = document.createElement("p")
+    cemail.innerHTML = pemail
+    email.appendChild(cemail)
+
+    let criaCpf = document.createElement("p")
+    criaCpf.innerHTML =  pcpf
+    cpf.appendChild(criaCpf)
+
+    let criaTel = document.createElement("p")
+    criaTel.innerHTML = ptelefoneWhats
+    telefone.appendChild(criaTel)
+
+    let criaCep = document.createElement("p")
+    criaCep.innerHTML = pcep
+    cep.appendChild(criaCep)
+
+    let criaEndereco = document.createElement("p")
+    criaEndereco.innerHTML = pendereco
+    endereco.appendChild(criaEndereco)
+
+    let criaNum = document.createElement("p")
+    criaNum.innerHTML = pnumero
+    numero.appendChild(criaNum)
+
+    let criaBairro = document.createElement("p")
+    criaBairro.innerHTML = pbairro
+    bairro.appendChild(criaBairro)
     
+    
+    btnSair.onclick = function () {
+        window.location.assign("/src/authentic3/authentic3.html")
+    }
 }
